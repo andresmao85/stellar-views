5ec041a8b30270418bbca739c07c972fe59ea0d1
Andres <andrex.ao@gmail.com>
2024-01-29 23:05:19 -0500

    first functional version

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,38 @@
-import Image from "next/image";
+"use client"
+
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
-  return (
-    <div>
-      <p></p>
-    </div>
-  )
+ const [apodData, setApodData]  = useState([]);
+ 
+
+ useEffect(() => {
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=cch1fD12dqyadllOoD2fXeQqM5GoW0t4l8WAkjSr&date=2014-10-01&concept_tags=True`)
+  .then((data) => data.json())
+  .then((data) => {
+    setApodData(data);
+    console.log(data)
+  })
+ }, []);
+ return ( 
+ <>
+  <div>
+    {apodData.media_type === "image" ? (
+    <img
+      src={apodData.url}
+      alt={apodData.title}
+      />
+  ) : (
+    <iframe
+      title="space-video"
+      src={apodData.url}
+      allow="autoplay"
+      allowFullScreen
+     />
+  )}
+
+  </div>
+ </>
+ )
 }
